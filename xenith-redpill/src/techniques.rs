ade2b097f40e859d73e0796f8eca3da54e68a9f1
abel <67806187+standard3@users.noreply.github.com>
2025-02-05 19:18:49 +0100

    feat: base modules

---
+++ xenith-redpill/src/techniques.rs
@@ -0,0 +1,26 @@
+/*
+Xenith - Xen-based security hypervisor
+Copyright (C) 2025 Xenith contributors
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+//! # Techniques
+//!
+//! This module contains the various techniques used to detect the presence of the Xen hypervisor
+//! by analyzing different aspects of the system.
+
+pub mod behavior;
+pub mod signature;
+pub mod time;
