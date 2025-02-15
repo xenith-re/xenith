063ce22c49480e6c4a3d00490c2552c6cce735f2
abel <67806187+theo-abel@users.noreply.github.com>
2025-02-15 17:02:36 +0100

    feat: added prelude module

---
+++ xenith-redpill/src/prelude.rs
@@ -0,0 +1,6 @@
+//! Re-exports of commonly used types and functions.
+//!
+//! This module contains re-exports of commonly used types and functions that are used throughout the crate.
+
+pub use crate::detector::run_all_techniques;
+pub use crate::detector::{DetectionResult, TechniqueError};
